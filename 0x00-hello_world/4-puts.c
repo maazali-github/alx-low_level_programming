1eab2d26f9961bd729ed0de286fb4793dce344cf
maazali-github <maazkharraj@gmail.com>
2024-06-20 20:53:37 +0300

    using gita

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
 * main - Entry
+*
 * Return: 0 if success
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
