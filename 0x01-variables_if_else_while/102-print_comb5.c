61882dfd2b3d04c08ee8867a43930b4a237e4c53
maazali-github <maazkharraj@gmail.com>
2024-06-21 18:03:15 +0300

    using gita

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	int firstNum = 0, secondNum;
+
+	while(firstNum <= 99)
+	{
+		secondNum = firstNum;
+		while (secondNum <= 99)
+		{
+			if (secondNum != firstNum)
+			{
+				putchar((firstNum / 10) + 48);
+				putchar((firstNum % 10) + 48);
+				putchar(' ');
+				putchar((secondNum / 10) + 48);
+				putchar((secondNum % 10) + 48);
+
+				if(firstNum != 98 || secondNum != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			secondNum++;
+		}
+		firstNum++;
+	}
+	putchar('\n');
+	return (0);
+}
