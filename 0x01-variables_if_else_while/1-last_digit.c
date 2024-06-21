c047a5accbb5bee0c000d272717067ce9fac75cf
maazali-github <maazkharraj@gmail.com>
2024-06-21 14:57:57 +0300

    using gita#

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
@@ -20,9 +21,7 @@ int main(void)
         printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
        else if (lastNum == 0)
         printf("Last digit of %d is %d and is 0\n", n, lastNum);
-      else
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
-
-	
-	return (0);
+             else
+                   printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
+      return (0);
 }
