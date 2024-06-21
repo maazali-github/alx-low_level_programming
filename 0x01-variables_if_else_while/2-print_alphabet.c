632fdc325ccfef81f5ca1167dae3350f66216f67
maazali-github <maazkharraj@gmail.com>
2024-06-21 15:15:46 +0300

    using gita

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
