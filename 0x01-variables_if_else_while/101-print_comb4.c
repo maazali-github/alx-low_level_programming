0eb896d9d58be34222ea7712132fc78b7dcb96f5
maazali-github <maazkharraj@gmail.com>
2024-06-21 17:44:55 +0300

    using gita

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,6 +6,39 @@
 */
 int main(void)
 {
+	int num1 = 0;
+	int num2, num3;
+
+	while ()
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			num3 = 0;
+			while (num3 <= 9)
+			{
+				if (num1 != num2 &&
+				   num1 < num2 &&
+				   num2 != num3 &&
+				   num2 < num3)
+				{
+					putchar(num1 + 48);
+					putchar(num2 + 48);
+					putchar(num3 + 48);
+
+					if (num1 + num2 + num3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				num3++;
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
 
 	return (0);
 }
