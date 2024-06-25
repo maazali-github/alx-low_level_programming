8a55b1a97ccb6118c3d5c25ef07d49663fbb4cea
root <root@DESKTOP-OI4Q969>
2024-06-25 23:46:12 +0300

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
-* print_sign - print + if n is greater than zero , 0 if n is zero and - if n is less than zero.
+* print_sign - print + if n is greater than zero , 
+* 0 if n is zero and - if n is less 
+* than zero.
 *
 * @n: takes integer type input for function.
 *
@@ -13,5 +15,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
 	}
 }
