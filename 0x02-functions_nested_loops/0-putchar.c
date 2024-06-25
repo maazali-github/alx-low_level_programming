79bf5f8503afabaf6a38065bf3952537c94f5451
root <root@DESKTOP-OI4Q969>
2024-06-25 13:15:39 +0300

    using gita

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+/**
 * main - Entry point
 * Description:'prints _putchar stdout'
 *Return: Always 0 (success)
@@ -6,7 +7,7 @@
 int main(void)
 {
 	_putchar('_');
-	_putschar('p');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
