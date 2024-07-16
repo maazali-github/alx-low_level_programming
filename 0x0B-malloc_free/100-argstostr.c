a44726b07d998cf91275f79e981611a338bbc500
root <root@DESKTOP-OI4Q969>
2024-07-16 12:07:06 +0300

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,50 +2,39 @@
 #include <stdio.h>
 
 /**
- * **strtow - splits a string into words
+ * *argstostr - concatenates all arguments of program
  *
- * @str: string to split
- * Return: pointer to array of strings, or NULL if fail
+ * @ac: argument count
+ * @av: point to pointer to arguments
+ * Return: pointer to new string or NULL if failed
  */
-char **strtow(char *str)
+char *argstostr(int ac, char **av)
 {
-	char **a;
-	int i, k, n, word_count = 0, word_len = 0, current_word = 0;
+	int i, k, n, size;
+	char *str;
 
-	if (str == NULL || *str == '\0')
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0; *(str + i) != '\0'; i++)
+	for (i = 0; i < ac; i++)
 	{
-		if (*(str + i) != ' ')
-			word_count++;
-		while (*(str + i) != ' ' && *(str + i))
-			i++;
-		if (!*(str + i))
-			break;
+		for (k = 0; av[i][k] != '\0'; k++)
+			;
+		size += k + 1;
 	}
-	if (word_count == 0)
+	str = malloc(size + 1);
+	if (str == 0)
 		return (NULL);
-	a = malloc(sizeof(char *) * (word_count + 1));
-	if (a == NULL)
-		return (NULL);
-	for (i = 0, k = i; current_word < word_count; i++, word_len = 0, k = i)
+	n = 0;
+	for (i = 0; i < ac; i++)
 	{
-		if (*(str + i) == ' ')
-			continue;
-		while (*(str + k) != ' ' && *(str + k++))
-			word_len++;
-		a[current_word] = malloc(sizeof(char) * word_len + 1);
-		if (!a[current_word])
+		for (k = 0; av[i][k] != '\0'; k++)
 		{
-			while (current_word-- >= 0)
-				free(a[current_word]);
-			free(a);
-			return (NULL);
+			str[n] = av[i][k];
+			n++;
 		}
-		for (n = 0; i < k; i++, n++)
-			a[current_word][n] = *(str + i);
-		a[current_word++][n] = '\0';
+		str[n] = '\n';
+		n++;
 	}
-	a[word_count] = NULL;
-	return (a);
+	return (str);
 }
+
