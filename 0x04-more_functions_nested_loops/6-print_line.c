9d6dc13298771b3124039e594fd7e1cb89c569dc
root <root@DESKTOP-OI4Q969>
2024-07-18 13:48:04 +0300

    print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,9 +6,13 @@
 */
 void print_line(int n)
 {
-while (n-- > 0)
-{
-_putchar('_');
-}
-_putchar('\n');
+	int lnchr;
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnchr = 1; lnchr <= n; lnchr++)
+		           _putchar('_');
+		_putchar('\n');
+	}
 }
